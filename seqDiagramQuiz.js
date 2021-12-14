58b03dbc40f6330a7191a50b560f80118b71dec0
Shiftadelic <65102155+Shiftadelic@users.noreply.github.com>
2021-12-14 15:46:45 -0500

    Matt's updates

--- seqDiagramQuiz.js
+++ seqDiagramQuiz.js
@@ -1,32 +1,3 @@
-// Questions will be asked
-/*
-const Questions = [{
-    id: 0,
-    question: "True or False?",
-    answers: [
-        { f: "False", isCorrect: false },
-        { t: "True", isCorrect: true }
-    ]
-}
-]
-*/
-
-//document.getElementById("checkBtn").addEventListener("click", checkAns)
-
-/*
-function toggleCheckbox()
-{
-  var toggleCb = document.getElementById("badTraitsBasic");
-  if(toggleCb.checked == true)
-  {
-    document.getElementById("badTraitsBasic").checked = false;
-  }
-  else
-  {
-    document.getElementById("badTraitsBasic").checked = true;
-  }
-}
-*/
 
 function checkAns()
 {
@@ -38,8 +9,7 @@ function checkAns()
   var correct = 0;
 
   
-  //for(var i = 0; i < 5; i++)
-  //{
+  
     var text = document.quiz.question2one.value;
     question2.push(text);
     text = document.quiz.question2two.value;
@@ -50,10 +20,7 @@ function checkAns()
     question2.push(text);
     text = document.quiz.question2five.value;
     question2.push(text);
-  //}
-
-  //var question2 = document.quiz.question2.value;
-  //var question3 = document.quiz.question3.value;
+ 
 
   if(question1 == "True")
   {
@@ -74,8 +41,7 @@ function checkAns()
       }
     }
   }
-  //var checkArr = false;
-  //checkArr = arraysEqual(question2, question2Ans);
+  
   if(correct == 5)
   {
     document.getElementById("correctMatch").innerHTML = "Correct!";
@@ -118,32 +84,3 @@ function checkAns()
   }
 }
 
-/*
-function arraysEqual(one, two)
-{
-  if (one === two) 
-  {
-    return true;
-  }
-  if (one == null || two == null) 
-  {
-    return false;
-  }
-  if (one.length !== one.length)
-  { 
-    return false;
-  }
-  // If you don't care about the order of the elements inside
-  // the array, you should sort both arrays here.
-  // Please note that calling sort on an array will modify that array.
-  // you might want to clone your array first.
-  for (var i = 0; i < a.length; ++i) 
-  {
-    if (a[i] !== b[i])
-    { 
-      return false;
-    }
-  }
-  return true;
-}
-*/
